8a1ca9c130e71390a1c74f6b4f8641c7a0d791ac
julia <nikitina.js@yandex.ru>
2023-08-03 18:11:45 +0400

    задача7 2 версия

--- task07/Program.cs
+++ task07/Program.cs
@@ -5,5 +5,12 @@
 
 Console.WriteLine("Ввведитен трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number*=-1;
+
+if (number >= 100 && number <=999)
+{
 int lastDigit = number%10;
 Console.WriteLine($"Последняя цифра {lastDigit}");
+}
+else Console.WriteLine("Ввудунот некорректное число");
