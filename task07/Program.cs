2eb703d188c0a16e151e2ef9dcdc48828a8a3240
julia <nikitina.js@yandex.ru>
2023-08-03 18:03:52 +0400

    задача7

---
+++ task07/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает на вход трехзначное число и на выход показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Ввведитен трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number%10;
+Console.WriteLine($"Последняя цифра {lastDigit}");
