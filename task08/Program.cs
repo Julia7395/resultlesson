9218dbe76907bcb4886eba8af83beee23265ad31
julia <nikitina.js@yandex.ru>
2023-08-05 21:02:33 +0400

    решение 8 задачи

--- task08/Program.cs
+++ task08/Program.cs
@@ -5,8 +5,8 @@
 
 Console.WriteLine("Введите число N");
 int N = Convert.ToInt32(Console.ReadLine());
-for (int i=0; i<=N; i++)
+for (int i=1; i<=N; i++)
 {
-if (i % 2 == 0);
-Console.WriteLine(i+ " ");
+if (i%2 == 0)
+Console.Write(i+ ",");
 }
