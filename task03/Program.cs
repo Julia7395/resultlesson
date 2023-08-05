ff66eec41bef2a3bebdbb52d1e52613488336169
julia <nikitina.js@yandex.ru>
2023-08-05 13:32:52 +0400

    решение 3 задачи

--- task03/Program.cs
+++ task03/Program.cs
@@ -3,7 +3,7 @@
 // 5 -> пятница
 
 Console.WriteLine ("Введите число от 1 до 7");
-string number = Console.ReadLine();
+string? number = Console.ReadLine();
 
 if (number == "1") Console.WriteLine("Понедельник");
 if (number == "2") Console.WriteLine("Вторник");
@@ -12,4 +12,4 @@ if (number == "4") Console.WriteLine("Четверг");
 if (number == "5") Console.WriteLine("Пятница");
 if (number == "6") Console.WriteLine("Суббота");
 if (number == "7") Console.WriteLine("Воскресенье");
-else if (number == "7") Console.WriteLine("Конец");
+else if (number == "8") Console.WriteLine("Конец");
