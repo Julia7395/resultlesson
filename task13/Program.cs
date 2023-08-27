5e262223d99b4d3a6e988676829f519a183b45ba
julia <nikitina.js@yandex.ru>
2023-08-27 13:48:23 +0400

    задача13

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int lastnambers(int numb)
+{
+   int lastnambers = numb/100 % 10;
+   return lastnambers;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lastnumber = lastnambers(number);
+Console.WriteLine("result");
+{  
+if (lastnumber == number/100 % 10) Console.WriteLine("lastnumber");
+else Console.WriteLine("Третьего числа нет");
+}
+
