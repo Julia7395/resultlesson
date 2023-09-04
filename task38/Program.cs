d5060beb4b5deb7d46f85bef43ac7e6d38e16ecb
julia <nikitina.js@yandex.ru>
2023-09-04 11:50:35 +0400

    задача38 - 1

---
+++ task38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(max - min) + min;
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+double DifMaxMin(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+        
+    }
+    double dif = max - min;
+    return dif;
+}
+
+double[] array = CreateArrayRndInt(4, 1, 100);
+PrintArray(array);
+double difMaxMin = DifMaxMin(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива = {difMaxMin}"); // НЕ ПОЛУЧАЕТСЯ ВЕЩЕСТВЕННОЕ ЧИСЛО.
