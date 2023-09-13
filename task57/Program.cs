14085f4bd79b4f25061eaa87fd2411265fe0f02c
julia <nikitina.js@yandex.ru>
2023-09-13 16:53:12 +0400

    задача57

---
+++ task57/Program.cs
@@ -0,0 +1,97 @@
+﻿//  Составить частотный словарь элементов 
+// двумерного массива. Частотный словарь содержит 
+// информацию о том, сколько раз встречается элемент 
+// входных данных.
+// { 1, 9, 9, 0, 2, 8, 0, 9 } 
+// 0 встречается 2 раза 
+// 1 встречается 1 раз 
+// 2 встречается 1 раз 
+// 8 встречается 1 раз 
+// 9 встречается 3 раза
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+         Console.Write("! ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int [] MatrixToArray(int[,] matrix)
+{
+    int[] array = new int[matrix.Length];
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[count++] = matrix[i,j];
+        }
+    }
+    return array;
+}
+
+void Dictionary(int[] array)
+{
+    int k = 1;
+    int num = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+       if (array[i] == num) k++; 
+       else 
+       {
+         Console.WriteLine($"{num} -> {k}");
+         num = array[i];
+         k = 1;
+       }
+
+    }
+    Console.WriteLine($"{num} -> {k}");
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(array2d);
+int[] arr = MatrixToArray(array2d);
+Array.Sort(arr);
+PrintArray(arr);
+Dictionary(arr);
+
+
